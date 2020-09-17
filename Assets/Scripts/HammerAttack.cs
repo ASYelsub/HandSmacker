077d69687d3aec413a4af2866ba22ea566e8b359
Abigail <asyelsub@gmail.com>
2020-09-17 15:08:27 -0400

    Figured out a dumb bug with euler angles and hammer movement.

---
+++ Assets/Scripts/HammerAttack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//this script makes the hammer attack down when the player presses space
+//attack down = rotating on the x axis one way and then another
+public class HammerAttack : MonoBehaviour
+{
+    private GameObject hingeObject;
+    private Transform hingeTransform;
+    private Vector3 initialRotation; //must be under zero! do not go over zero! it will fuck up the math! fuck eulers!
+    [SerializeField]
+    private Vector3 hitRotation; //the min number that the hammer can be at before it goes back
+    [SerializeField] 
+    private Vector3 hitDownSpeed;
+
+    private Vector3 movementVector;
+    
+    public bool hammerIsAttacking;
+    
+
+    private void Start()
+    {
+        hammerIsAttacking = false;
+        hingeObject = gameObject;
+        hingeTransform = hingeObject.transform;
+        initialRotation = hingeObject.transform.eulerAngles;
+    }
+
+    private void Update()
+    {
+        if (!hammerIsAttacking)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // print("HI THIS FUCKIN HAPPENED");
+                hammerIsAttacking = true;
+            }   
+        }
+        else if (hammerIsAttacking)
+        {
+            //print("now we are true");
+            if (hingeObject.transform.eulerAngles.x - 360f <= hitRotation.x) //watch the fuck out for euler angles, bro.
+            {
+                hammerIsAttacking = false;
+                print("hammer finished attacking");
+            }
+            else
+            {
+                //print("in here???");
+                movementVector = hitDownSpeed;
+                hingeObject.transform.eulerAngles += movementVector;
+            }
+            //print(hingeObject.transform.eulerAngles.x - 360f); 
+            
+        }
+    }
+}
--- Assets/Scripts/HammerMovement.cs
+++ Assets/Scripts/HammerMovement.cs
@@ -4,18 +4,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = System.Random;
 
-//this script moves the hammer back and forth on the world x axis according to the hinge.
+//This script moves the hammer back and forth at random speeds on the world x axis according to the hinge.
+//This script stops movement momentarily when the bool hammerIsAttacking is true on HammerAttack.
 public class HammerMovement : MonoBehaviour
 {
     private GameObject hingeObject;
     private Transform hingeTransform;
     private Vector3 movementVector;
-    
+    [Header("Other Scripts")]
+    [SerializeField]
+    private HammerAttack hammerAttack;
     [Header("Adjustments")]
     [SerializeField]
     private float xMax;
     [SerializeField]
     private float xMin;
+    //These two should probably be around .01 to .3
+    [SerializeField] private float movementSpeedMinimum;
+    [SerializeField] private float movementSpeedMaximum;
+
+    private float movementSpeed;
 
     //this is the bool that is true if the hammer's x pos is increasing.
     private bool goingRight; 
@@ -23,12 +31,13 @@ public class HammerMovement : MonoBehaviour
     {
         hingeObject = gameObject;
         hingeTransform = hingeObject.transform;
-        
+
         float randomizer = UnityEngine.Random.Range(0f, 2f);
         if (randomizer > 1f)
             goingRight = true;
         else if (randomizer <= 1f)
             goingRight = false;
+        RandomizeMovementSpeed();
     }
 
     private void FixedUpdate()
@@ -41,7 +50,8 @@ public class HammerMovement : MonoBehaviour
             }
             else if (hingeTransform.position.x < xMax)
             {
-                movementVector = new Vector3(1,0,0);
+                RandomizeMovementSpeed();
+                movementVector = new Vector3(movementSpeed,0,0);
             }
         }
         if (!goingRight)
@@ -52,9 +62,15 @@ public class HammerMovement : MonoBehaviour
             }
             else if (hingeTransform.position.x > xMin)
             {
-                movementVector = new Vector3(-1,0,0);
+                RandomizeMovementSpeed();
+                movementVector = new Vector3(-movementSpeed,0,0);
             }
         }
         hingeObject.transform.position = hingeObject.transform.position + movementVector;
+    }
+
+    private void RandomizeMovementSpeed()
+    {
+        movementSpeed = UnityEngine.Random.Range(movementSpeedMinimum, movementSpeedMaximum);
     }
 }
