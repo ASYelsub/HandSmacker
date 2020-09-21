b40003fee743a130df88b2a2cf0a8095e139b743
Abigail <asyelsub@gmail.com>
2020-09-20 22:35:37 -0400

    I have the screen changes working but I need to make some changes to how the score is calculated. Apparently there is no point in doing well. But... that's sort of the point. Ugh I'm not super sure what to do.

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour
+{
+    
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
