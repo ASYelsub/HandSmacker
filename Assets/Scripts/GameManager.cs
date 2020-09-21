b86b83d823f46c5f533b9e13bc90d50203f3613c
Abigail <asyelsub@gmail.com>
2020-09-20 21:22:34 -0400

    Added conveyer model, making some changes with spawning it

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private float timer = 0;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            timer+= Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+        }
+
+        if (timer >= 4f)
+        {
+            SceneManager.LoadScene("EndGame");
+        }
+    }
+}
